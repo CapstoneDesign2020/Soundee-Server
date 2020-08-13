194305a305d024853a22fc7962cdc9bf88dbef41
wlgp2500 <wlgp2500@naver.com>
2020-08-13 13:01:57 +0900

    get daily, weekly, monthly data complete!

--- models/chart.js
+++ models/chart.js
@@ -3,11 +3,32 @@ const table = 'sound';
 
 const chart = {
     getChart : async (userIdx)=>{
-        const daily_query = `select class,date_format(eventdate,'%Y%m%d') as 'date',count(date_format(eventdate,'%Y%m%d')) as 'value' from ${table} where sound_userIdx = ${userIdx} and (eventdate between date_add(NOW(),INTERVAL -1 DAY ) AND NOW()) group by class;`;
+        const daily_query = `select class,date_format(eventdate,'%Y%m%d') as 'date',count('date') as 'value' 
+                                from ${table} 
+                                where sound_userIdx = ${userIdx} and (date_format(eventdate,'%Y%m%d') = date_format(NOW(),'%Y%m%d') ) group by class, date;`;
+        const weekly_query = `select class,date_format(eventdate,'%Y%m%d') as 'date',count(date_format(eventdate,'%Y%m%d')) as 'value'
+                                from ${table}
+                                where sound_userIdx =${userIdx} and (eventdate between date_add(NOW(),INTERVAL -1 WEEK ) AND NOW())
+                                group by class, date 
+                                order by class, date asc;`;
+        const monthly_query =`select class,date_format(eventdate,'%Y%m') as 'date',count(date_format(eventdate,'%Y%m')) as 'value'
+                                from ${table}
+                                where sound_userIdx =${userIdx} and (eventdate between date_add(NOW(),INTERVAL -1 year ) AND NOW())
+                                group by class, date 
+                                order by class, date asc;`
         try{
-            const result = await pool.queryParam(daily_query);
-            console.log(result);
-            return result;
+            let total_chart = new Object();
+
+            const daily_result = await pool.queryParam(daily_query);
+            const weekly_result = await pool.queryParam(weekly_query);
+            const monthly_result = await pool.queryParam(monthly_query);
+
+            total_chart.daily = daily_result;
+            total_chart.weekly = weekly_result;
+            total_chart.monthly = monthly_result;
+
+            console.log(total_chart.daily[0].class);
+            return total_chart;
         }catch(err){
             if (err.errno == 1062) {
                 console.log('getChart ERROR : ', err.errno, err.code);
